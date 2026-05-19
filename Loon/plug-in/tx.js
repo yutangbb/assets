10a933f6dfed9c6c175f97b5a155734c7d576a07
YUTANG <yutangbb@gmail.com>
2026-05-20 06:19:27 +0800

    Update tx.js

--- Loon/plug-in/tx.js
+++ Loon/plug-in/tx.js
@@ -1,2 +1,8 @@
+console.log("\n");
+console.log($argument.arg1);
+console.log("\n");
+console.log($argument.arg2);
+console.log($response.body);
+console.log("\n");
 const body = $response.body.replace("<body>", `<body><script>localStorage.setItem("fuck",${$argument.arg1});localStorage.setItem("sun",${$argument.arg2});</script>`);
 $done({ body: body });
