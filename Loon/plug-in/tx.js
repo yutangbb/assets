fa07104b4f2f9e025138f6da74ac5ddc8d02e8d2
YUTANG <yutangbb@gmail.com>
2026-05-20 06:08:47 +0800

    Add files via upload

--- Loon/plug-in/tx.js
+++ Loon/plug-in/tx.js
@@ -1,4 +1,2 @@
-(function () {
-    const body = $response.body.replace("<body>", `<body><script>localStorage.setItem("fuck",${$argument.arg1});localStorage.setItem("sun",${$argument.arg2});</script>`);
-    $done({body:body});
-})();
+const body = $response.body.replace("<body>", `<body><script>localStorage.setItem("fuck",${$argument.arg1});localStorage.setItem("sun",${$argument.arg2});</script>`);
+$done({ body: body });
