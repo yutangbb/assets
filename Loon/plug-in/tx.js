6c6c8016c5dd02273819d6a298a81cec869cc55e
YUTANG <yutangbb@gmail.com>
2026-05-20 11:44:10 +0800

    Update tx.js

--- Loon/plug-in/tx.js
+++ Loon/plug-in/tx.js
@@ -2,10 +2,11 @@ console.log("\n");
 console.log($argument.arg1);
 console.log($argument.arg2);
 
-const body = $response.body.replace("<body>", `<body><script>localStorage.setItem("fuck","${$argument.arg1}");localStorage.setItem("sun","${$argument.arg2}");</script>`);
+const body = $response.body.replace("<body>", `<body><script>localStorage.setItem("fuck","${$argument.arg1}");localStorage.setItem("sun","${$argument.arg2}");</script><style>.video-item-combination.video-item.waterfall:-abp-has(>.aspect-ratio>.overlay>a),.video-item-combination.van-col:-abp-has(>.video-item:first-child:last-child>.aspect-ratio>.overlay>a){display:none!important;}<style/>`);
 
 console.log("\n");
 console.log(body);
 console.log("\n");
 
 $done({ body: body });
+
